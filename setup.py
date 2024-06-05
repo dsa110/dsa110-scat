4434358299c895f8ed2aed90deb0a63dee6b8d2a
Jakob Faber <jakobtfaber@gmail.com>
2024-06-05 13:46:43 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+"""
+try:
+    version = get_git_version()
+    assert version is not None
+except (AttributeError, AssertionError):
+    version = '1.0.0'
+"""
+
+setup(name='dsa-110_scat-dev',
+      version='1.0.0',
+      description='DSA-110 Scattering and Scintillation Utilities',
+      packages=['scattering','scintillation'],
+      install_requires=[
+          'numpy',
+          'matplotlib',
+          'sigpyproc',
+          'lmfit',
+          'bilby',
+          'astropy'
+          'scipy',
+          'pandas',
+          'tqdm'
+          ],
+
+     )
